e3e4d0a24c00224e02fad359a54348e17db4a50f
Melvin Berena <melvin.berena@gmail.com>
2016-05-20 08:58:33 -0400

    Initial Setup

---
+++ src/BooksAngularApp/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksAngularApp.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Type { get; set; }
+        public DateTime DatePublished { get; set; }
+        public decimal Price { get; set; }
+    }
+}
